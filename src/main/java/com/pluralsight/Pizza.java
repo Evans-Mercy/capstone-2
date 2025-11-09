96a4e14d2c7bab3c492159821ad0adc553520c3a
Evans-Mercy <cheria.mercy@gmail.com>
2025-11-09 15:05:33 -0600

    Initial commit

---
+++ src/main/java/com/pluralsight/Pizza.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Pizza {
+    private String size;
+    private String crustType;
+    private boolean stuffed;
+    private ArrayList<String> toppings;
+}
